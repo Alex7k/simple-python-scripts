7fd2c295528c7a478b2409c518d410c4ee9fc639
@alexander.moeri <alexander.moeri@oerlikon.com>
2025-05-28 10:44:42 +0200

    add collatz conjecture script

---
+++ collatzconjecture.py
@@ -0,0 +1,34 @@
+# https://en.wikipedia.org/wiki/Collatz_conjecture
+
+# Even number: / 2
+# Odd number: * 3 + 1
+
+samples = 100000
+leaderboard = []
+for startingnumber in range(1, samples):
+  # measure how many steps to end up in the 4, 2, 1 loop
+  step = 0
+  highestnumber = 1
+  number = startingnumber
+  while True:
+    step += 1
+    if number % 2 == 0:
+      number = number // 2
+    elif number % 2 == 1:
+      number = number * 3 + 1
+    if number > highestnumber:
+      highestnumber = number
+    # print(number)
+    if number == 1:
+      # print(f"{startingnumber} arrived at loop at step {step} with highest number being {highestnumber}")
+      leaderboard.append([startingnumber, step, highestnumber])
+      break
+
+print("Leaderboard:")
+print("Starting number, achieved step, highest number")
+leaderboard.sort(key=lambda x: x[2], reverse=True)
+leaderboardlength = len(leaderboard)
+if 10 < leaderboardlength:
+  leaderboardlength = 10
+for i in range(leaderboardlength):
+  print(leaderboard[i])
