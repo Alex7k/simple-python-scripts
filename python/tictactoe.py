babcffe2cdd27d4844979b14b2bafb946327aeb6
Alex <51133872+Alex7k@users.noreply.github.com>
2026-04-14 15:57:13 +0200

    update tic tac toe

--- python/tictactoe.py
+++ python/tictactoe.py
@@ -8,46 +8,50 @@ players = ["X", "O"]
 def printboard():
   for i in range(3):
     print(f"{board[0+i*3]} | {board[1+i*3]} | {board[2+i*3]} ")
-    if i < 2: # avoid printing line after last row
+    if i < 2: # Don't print line after last row
       print("--|---|--")
 
-def checkwin(player):
-  for row in range(3):
-    if all(board[row*3 + col] == player for col in range(3)):
-      return True
-  for col in range(3):
-    if all(board[row*3 + col] == player for row in range(3)):
-      return True
-  if board[0] == board[4] == board[8] == player or board[2] == board[4] == board[6] == player:
-    return True
-  return False
+def checkwin():
+    for player in players:
+        for i in range(3):
+            # Row
+            if board[0+i*3] == board[1+i*3] == board[2+i*3] == player:
+               return player
+            # Column
+            if board[0*3+i] == board[1*3+i] == board[2*3+i] == player:
+                return player
+            # Diagonal
+            if board[0] == board[4] == board[8] == player or board[2] == board[4] == board[6] == player:
+               return player
+    return False
+               
 
-def checkdraw():
-  return all(board[i] in players for i in range(9))
+def checkstale():
+    for i in board:
+       if i not in players:
+          return False
+    return True
 
 printboard()
 
 while True:
-  for player in players:
-    playerinput = "undefined"
-    while True:
-      try:
-        playerinput = int(input(f"Player {player}, select a position (1-9): "))
-        if not (1 <= playerinput <= 9) or board[playerinput-1] in players:
-          print("Invalid position!")
-          continue
-        else:
-          board[playerinput-1] = player
-          break
-      except ValueError:
-        print("Please enter a valid number!")
-        continue
+    for player in players:
+        while True:
+            try:
+                chosen_pos = int(input("Wähle ein Feld  ")) - 1
+                if board[chosen_pos] not in players:
+                    board[chosen_pos] = player
+                    break
+            except (ValueError, IndexError):
+               pass
+            print("Invalid position!")
+        printboard()
 
-    printboard()
-
-    if checkwin(player):
-      print(f"{player} wins!")
-      exit()
-    if checkdraw():
-      print("Draw!")
-      exit()
+        winstatus = checkwin()
+        if winstatus:
+           print(f"Player {winstatus} won!")
+           exit()
+        elif checkstale():
+           print("Stalemate!")
+           exit()
+           
